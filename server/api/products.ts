31db784f0cddce48fc17fefc251ed7febdddbca8
huevangxp <huevang933@gmail.com>
2026-08-11 15:26:43 +0700

    commit Tue 08/11/2026 15:26:43.13

--- server/api/products.ts
+++ server/api/products.ts
@@ -3,13 +3,13 @@ export default defineEventHandler(async (event) => {
     const query = getQuery(event)
     const kw = query.kw ?? ''
     const catId = query.catId ?? 0
-    const flagId = query.flagId ?? 1
+    const flagId = query.flagId ?? 0
     const brandId = query.brandId ?? 0
     const price = query.price ?? 0
     const skip = query.skip ?? 0
-    const count = query.count ?? 18
+    const count = query.count ?? 24
 
-    const url = `https://api.olaa.la/api/store/products?kw=${kw}&catId=${catId}&flagId=${flagId}&brandId=${brandId}&price=${price}&skip=${skip}&count=${count}`
+    const url = `https://api.olaa.la/api/store/products?kw=${encodeURIComponent(kw)}&catId=${catId}&flagId=${flagId}&brandId=${brandId}&price=${price}&skip=${skip}&count=${count}`
     const data = await $fetch(url)
     return data
   } catch (error) {
