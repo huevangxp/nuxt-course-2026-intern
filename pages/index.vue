b80ac4ed4b24c4a9941b9f6609c344a36b1e4a8a
huevangxp <huevang933@gmail.com>
2026-08-11 15:18:07 +0700

    commit Tue 08/11/2026 15:18:07.18

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,8 @@
                 <v-card hover flat border class="fill-height d-flex flex-column overflow-hidden">
                     <div class="bg-grey-lighten-4 pa-6 text-center d-flex align-center justify-center"
                         style="height: 140px;">
-                        <v-img :src="brand.logoUrl" aspect-ratio="1" cover width="100" height="100"></v-img>
+                        <v-img :src="'https://api.olaa.la/v1' + brand.logoUrl" aspect-ratio="1" cover width="100"
+                            height="100"></v-img>
                     </div>
                     <v-card-title class="font-weight-bold text-subtitle-1 px-4 pt-3 pb-1">
                         {{ brand.name }}
