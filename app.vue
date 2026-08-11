906d7e24c1d6920f3ad117c01f7de40ca8fa6300
huevangxp <huevang933@gmail.com>
2026-08-11 14:37:00 +0700

    commit Tue 08/11/2026 14:37:00.21

--- app.vue
+++ app.vue
@@ -3,3 +3,9 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+body, button, input, select, textarea, .v-application {
+  font-family: 'Noto Sans Lao', 'Noto Sans', sans-serif !important;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,25 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@300;400;500;600;700&family=Noto+Sans:wght@300;400;500;600;700&display=swap'
+        }
+      ]
+    }
+  },
   modules: ['vuetify-nuxt-module'],
   vuetify: {
     /* Vuetify options */
